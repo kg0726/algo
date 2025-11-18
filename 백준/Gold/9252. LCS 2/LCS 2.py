b0b27c08aabd00f5a547b38d7bef48f2d0312516
kg0726 <kjmin065999@naver.com>
2025-11-18 15:48:27 +0900

    [Gold IV] Title: LCS 2, Time: 128 ms, Memory: 118460 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,48 @@
+# 입력 시작
+first = input()
+second = input()
+
+f = len(first)
+s = len(second)
+
+# 2차원 dp배열 생성
+dp = [[0] * (s + 1) for _ in range(f + 1)]
+memo = set()
+
+
+# f와 s를 2차원 순회
+for i in range(1, f + 1):
+    for j in range(1, s + 1):
+        # 순회 중 i, j의 알파벳이 같은 경우
+        # 현재 알파벳을 lcs의 마지막 알파벳으로 사용할 수 있다는 뜻
+        # 즉, lcs의 최장 거리가 현재 시점에서 1 증가한다는 뜻으로
+        # 현재 알파벳을 사용하지 않았을 최장거리를 가지고 있는
+        # dp[i - 1][j - 1]에 1을 더해 현재 위치에 할당함
+        if first[i - 1] == second[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        # 알파벳이 다른 경우
+        # 현재 알파벳을 lcs의 마지막 알파벳으로 사용할 수 없다는 뜻
+        # 즉, lcs의 최장거리에는 변함이 없음
+        # 그렇다면 현재 선택된 두 알파벳 중 하나를 사용하지 않았을 시점의 최장거리를 할당
+        elif first[i - 1] != second[j - 1]:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+lcs = []
+
+print(dp[f][s])
+if dp[f][s] > 0:
+    i, j = f, s
+    while i > 0 and j > 0:
+        # 문자가 같으면 LCS의 구성글자임
+        if first[i - 1] == second[j - 1]:
+            lcs.append(first[i - 1])
+            i -= 1
+            j -= 1
+        else:
+            # 위쪽 값이 더 크면 위로 이동
+            if dp[i - 1][j] >= dp[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+    lcs.reverse()
+    print(''.join(lcs))
